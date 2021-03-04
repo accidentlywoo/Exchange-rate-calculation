58fe7969a355187c6a527af6e4cc363daf0f70ee
accidentlywoo <accidentlywoo@gmail.com>
2021-03-05 00:46:16 +0900

    H2 디비연결 테스트 완료 및 프론트 부트스트랩 뚝딱

---
+++ exchange_rate/src/main/java/com/exchange/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.exchange.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+	@GetMapping("/")
+	public String welcompage(){
+		return "index";
+	}
+}
